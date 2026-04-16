842aa779f58b7c150abdd51bbc45321459efed38
Boomija Balasubramanian <boomijabalasubramanian03@gmail.com>
2026-04-16 12:09:59 +0530

    Add grading system based on student marks

---
+++ ex1/grade.py
@@ -0,0 +1,13 @@
+mark = float(input("ENTER THE MARK OF THE STUDENT:"))
+if mark>=91 and mark<=100:
+   print("O GRADE")
+elif mark>=81 and mark<=90:
+   print("A+ GRADE")
+elif mark>=71 and mark<=80:
+   print("A GRADE")
+elif mark>=61 and mark<=70:
+   print("B+ GRADE")
+elif mark>=51 and mark<=60:
+   print("B GRADE")
+else:
+   print("RE-APPEAR")
