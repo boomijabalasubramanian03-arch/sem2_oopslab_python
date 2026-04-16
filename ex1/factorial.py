0b212bf5b31693e861f500150f6d39d9cc65a310
Boomija Balasubramanian <boomijabalasubramanian03@gmail.com>
2026-04-16 12:13:49 +0530

    Add factorial calculation in factorial.py
    Implement factorial calculation using both for and while loops.

---
+++ ex1/factorial.py
@@ -0,0 +1,11 @@
+n1 = int(input("ENTER THE NUMBER:"))
+fact = 1
+for i in range(1,n1+1):
+   fact = fact*i
+print ("Factorial of",n1,"is",fact)
+n2 = int(input("ENTER THE NUMBER:"))
+f = 1;i=1
+while i<=n2:
+   f = f*i
+   i+=1
+print ("Factorial of",n2,"is",f)
